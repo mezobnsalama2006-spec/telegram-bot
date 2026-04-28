106ba407fb7681ddbf117b1bbb26f453361c5328
mezobnsalama2006-spec <mezobnsalama2006@gmail.com>
2026-04-28 18:01:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = "8759960592:AAHMD99wwGwiD5u1GTDXG0zZXjBpXWpjPBU"
 
 # Your Telegram user ID(s) - to get yours, message @userinfobot
 ADMIN_IDS = [
-    6098463726,   # Replace with your Telegram ID
+    7428181415,   # Replace with your Telegram ID
     # Add more admin IDs here
 ]
 
@@ -15,9 +15,10 @@ ADMIN_IDS = [
 # PAYMENT INFO (shown to users when adding balance)
 # Edit these with your real payment details
 # ========================
-PAYMENT_BYBIT= "496950466"
+PAYMENT_BYBIT= "496950466 .  546961050  "
 PAYMENT_INSTAPAY = "01028749936"
-PAYMENT_BINANCE = "1199904304"
+PAYMENT_BINANCE = "1199904304 .  422086209 "
 
 # Support username
-SUPPORT_USERNAME = "@Mustafa_Bn_Salama"
+SUPPORT_USERNAME = "@KDB_store_Admin
+"
